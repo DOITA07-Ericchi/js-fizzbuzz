4644712f0984b4e0cfb8043e5effa62982464612
EricchiYukia <git@ericchi.moe>
2022-11-11 16:47:05 +0100

    Base e un po' di JS

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-let arrayMacchine = ["Maserati", "Bmw", "Audi", "Fiat"];
+let containerPrincipale = document.querySelector(".mio-container");
 
-for(let i = 0; i < arrayMacchine.length; i++){
-    console.log(arrayMacchine[i]);
-}
-
-arrayMacchine[2] = "Ferrari";
-
-for(let i = 0; i < arrayMacchine.length; i++){
-    
-    if(arrayMacchine[i] == "Ferrari" || arrayMacchine[i] == "Maserati" ){
-        console.log("EVVIVA HAI UN AUTO BELLA NEL TUO PARCO AUTO!");
-    } else {
-        console.log(arrayMacchine[i]);
-    }
-}
-
-
-
-let arrayNumeri = [5, 9, 2, 6, 4];
-
-for(let i = 0; i< arrayNumeri.length; i++){
-    console.log(arrayNumeri[i]);
-}
-
-for(let i = 0; i< arrayNumeri.length; i++){
-
-    if(arrayNumeri[i]<5){
-        console.log("Il numero è minore di 5");
-    } else{
-        console.log("Il numero è maggiore uguale a 5");
-    }
-    
+for (let i = 0; i < 100; i++ ){
+        if (i == 5 && i == 3) {
+            console.log("fizzbuzz")
+        }
 }
