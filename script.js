3cfb449ff0052dd436c62d599122005f8de52978
EricchiYukia <git@ericchi.moe>
2022-11-11 17:04:26 +0100

    Finito.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let containerPrincipale = document.querySelector(".mio-container");
+let output;
 
-for (let i = 0; i < 100; i++ ){
+for (let i = 1; i < 101; i++ ){
         if (i % 5 == 0 && i % 3 == 0) {
-            console.log("fizzbuzz");
+            output = "fizzbuzz";
         } else if (i % 5 == 0) {
-            console.log("buzz");
+            output = "buzz";
         } else if (i % 3 == 0) {
-            console.log("fizz");
+            output = "fizz";
         } else {
-            console.log(i+1);
+            output = i;
         }
+        console.log(output);
+        containerPrincipale.innerHTML += '<div class="square"><p>' + output + '</p></div>';
 }
